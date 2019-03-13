0df8c51039b025ea61364d457e9e8802f8880fce
quintuslabs <quintus.labs@gmail.com>
2019-03-13 19:00:55 +0530

    new file added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -27,3 +27,24 @@ $(document).ready(function() {
     })
 });
 
+$(function() {
+  $('progress').each(function() {
+    var max = $(this).val();
+    $(this).val(0).animate({ value: max }, { duration: 2000, easing: 'easeOutCirc' });
+      });
+});
+
+var delay = 500;
+$(".progress-bar").each(function(i){
+    $(this).delay( delay*i ).animate( { width: $(this).attr('aria-valuenow') + '%' }, delay );
+
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: delay,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now)+'%');
+        }
+    });
+});
